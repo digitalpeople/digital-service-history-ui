ac3bf83855098c3ca7f199a83a7461ef57e78c20
Adam Hegedus <adam.hegedus@yandex.ru>
2019-07-22 14:49:20 +0200

    Add project skeleton

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": ["react-app", "airbnb"],
+  "rules" : {
+    "indent": ["error", 2],
+    "operator-linebreak": [2, "after"],
+    "react/jsx-filename-extension": [
+      1, 
+      {
+        "extensions": [".js", ".jsx"],
+      },
+    ],
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import About from './about';
+import Home from './home';
+
+const App = () => (
+  <div className="App">
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
+  </div>
+);
+
+export default App;
