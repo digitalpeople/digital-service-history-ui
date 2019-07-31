6e4430fd485669db3235e103f34495b9ccd34f68
Adam Hegedus <adam_hegedus@epam.com>
2019-07-31 16:07:48 +0200

    Refactor form inputs to component

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,39 +1,22 @@
 import React from 'react';
+import TextInput from '../forms/text-input';
 import './login.scss';
 
 const Login = () => (
   <section className="login">
     <header>Login</header>
     <article className="container">
-      <div className="form-group">
-        <input
-          type="text"
-          name="username"
-          className="form-control"
-          required
-        />
-        <span
-          htmlFor="username"
-          className="label"
-        >
-          Username
-        </span>
-      </div>
+      <TextInput
+        name="username"
+        label="Username"
+      />
 
-      <div className="form-group">
-        <input
-          type="password"
-          name="password"
-          className="form-control"
-          required
-        />
-        <span
-          htmlFor="password"
-          className="label"
-        >
-          Password
-        </span>
-      </div>
+      <TextInput
+        name="password"
+        label="Password"
+        isPassword
+      />
+
       <button type="submit">Login</button>
 
     </article>
