83ffe1f152185a3a4a222dd0270488b9056b9b9d
Adam Hegedus <limitlessaudio@gmail.com>
2019-08-03 23:10:33 +0200

    Setup axios for login

--- src/components/login/login.action.js
+++ src/components/login/login.action.js
@@ -1,13 +1,29 @@
+import axios from 'axios';
 import {
   login,
   logout,
 } from './action-types';
 
-export const loginAction = ({ username, password }) => ({
-  type: login,
-  username,
-  password,
-});
+export const loginAction = ({ username, password }) => {
+  axios.post('/auth/login', {
+    username,
+    password,
+  })
+    .then((response) => {
+      localStorage.setItem('userToken', response.data.hash);
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.err('error', error);
+      localStorage.removeItem('userToken');
+    });
+
+  return {
+    type: login,
+    username,
+    password,
+  };
+};
 
 export const logoutAction = () => ({
   type: logout,
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import store from './store';
 import * as serviceWorker from './serviceWorker';
+
+axios.defaults.baseURL = 'http://localhost:9000';
 
 ReactDOM.render(
   <Provider store={store}>
